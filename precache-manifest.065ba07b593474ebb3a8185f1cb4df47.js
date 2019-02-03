0dee46b8acdde7fa1e556ca32f831c4ced88f8bd
root <root@rtc.netbriefings.com>
2019-02-02 23:59:12 -0500

    Updates

---
+++ precache-manifest.065ba07b593474ebb3a8185f1cb4df47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6485b998b4d4299b44e",
+    "url": "/chinese_new_year/static/js/runtime~main.b6485b99.js"
+  },
+  {
+    "revision": "386f278c7c0dbb41d75e",
+    "url": "/chinese_new_year/static/js/main.386f278c.chunk.js"
+  },
+  {
+    "revision": "e01e2dfdc9f3e8fd49e3",
+    "url": "/chinese_new_year/static/js/1.e01e2dfd.chunk.js"
+  },
+  {
+    "revision": "386f278c7c0dbb41d75e",
+    "url": "/chinese_new_year/static/css/main.7c4e3147.chunk.css"
+  },
+  {
+    "revision": "55f3911c282073f6a084c49d63ea2b2a",
+    "url": "/chinese_new_year/index.html"
+  }
+];
